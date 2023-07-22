dd648c8db06c312be191a32cc1cff0af249eef96
mr-dorian-black <mr-dorian-black@users.noreply.github.com>
2023-07-22 22:07:37 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+let instance;
+
+const openModal = (link) => {
+     instance = basicLightbox.create(`<img src="${link}">`);
+     document.addEventListener('keydown', closeModal);
+     instance.show();
+}
+
+
+const images = galleryItems.map(item => {
+     let liItem = document.createElement('li');
+     liItem.classList.add('gallery__item');
+
+     let aItem = document.createElement('a');
+     aItem.classList.add('gallery__link');
+     aItem.href = item.original;
+
+     let imgItem = document.createElement('img');
+     imgItem.classList.add('gallery__image');
+     imgItem.src = item.preview;
+     imgItem.dataset.source = item.original;
+     imgItem.alt = item.description;
+
+     aItem.append(imgItem);
+     liItem.append(aItem);
+     return liItem
+});
+
+gallery.append(...images);
+
+
+function openImage(event) {
+     event.preventDefault();
+     openModal(event.target.dataset.source);
+}
+
+function closeModal(event) {
+     if (event.code === 'Escape') {
+          instance.close();
+          document.removeEventListener('keydown', closeModal);
+     }
+}
+
+gallery.addEventListener('click', openImage);
